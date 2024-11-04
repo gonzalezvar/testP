ff9a49e90ce035f39aecf0ed861528696982fde8
gonzalezvar <71093867+gonzalezvar@users.noreply.github.com>
2024-11-04 19:51:53 +0000

    otro ejemplo de clase

--- test-fetch/src/App.jsx
+++ test-fetch/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDo, setToDo] = useState([])
+
+  const loadToDos = async () => {
+    try {
+      const response = await fetch('https://playground.4geeks.com/todo/users/juan')
+      const result = await response.json()
+      setToDo(result.todos)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const saveToDo = async () => {
+    try {
+      const body = {
+        "label": "tarea #4",
+        "is_done": false
+      }
+      const response = await fetch("https://playground.4geeks.com/todo/todos/juan", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      const result = await response.json()
+      setToDo(prevState=>[...prevState,result])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    // saveToDo();
+    loadToDos()
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {toDo.map(item => (
+          <li>{item.label}</li>
+        ))}
+      </ul>
+      <button onClick={saveToDo}>Añadir nueva tarea</button>
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
